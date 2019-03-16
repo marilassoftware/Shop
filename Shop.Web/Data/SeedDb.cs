6482678923ca2c9961e7f5bf44679e5197bb71b7
Marilas Software <mrilassoftware@gmail.com>
2019-03-16 18:22:38 -0500

    Se crea una manera para ingresar datos a la base de datos.

---
+++ Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,109 @@
+﻿namespace Shop.Web.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+    //using Helpers;
+    using Microsoft.AspNetCore.Identity;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        //private readonly IUserHelper userHelper;
+        private readonly Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            //this.userHelper = userHelper;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            //await this.userHelper.CheckRoleAsync("Admin");
+            //await this.userHelper.CheckRoleAsync("Customer");
+
+            //if (!this.context.Countries.Any())
+            //{
+            //    var cities = new List<City>();
+            //    cities.Add(new City { Name = "Medellín" });
+            //    cities.Add(new City { Name = "Bogotá" });
+            //    cities.Add(new City { Name = "Calí" });
+
+            //    this.context.Countries.Add(new Country
+            //    {
+            //        Cities = cities,
+            //        Name = "Colombia"
+            //    });
+
+            //    await this.context.SaveChangesAsync();
+            //}
+
+
+            //// Add user
+            //var user = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+            //if (user == null)
+            //{
+            //    user = new User
+            //    {
+            //        FirstName = "Juan",
+            //        LastName = "Zuluaga",
+            //        Email = "jzuluaga55@gmail.com",
+            //        UserName = "jzuluaga55@gmail.com",
+            //        PhoneNumber = "350 634 2747",
+            //        Address = "Calle Luna Calle Sol",
+            //        CityId = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault().Id,
+            //        City = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault()
+            //    };
+
+            //    var result = await this.userHelper.AddUserAsync(user, "123456");
+            //    if (result != IdentityResult.Success)
+            //    {
+            //        throw new InvalidOperationException("Could not create the user in seeder");
+            //    }
+
+            //    await this.userHelper.AddUserToRoleAsync(user, "Admin");
+            //    var token = await this.userHelper.GenerateEmailConfirmationTokenAsync(user);
+            //    await this.userHelper.ConfirmEmailAsync(user, token);
+            //}
+
+            //var isInRole = await this.userHelper.IsUserInRoleAsync(user, "Admin");
+            //if (!isInRole)
+            //{
+            //    await this.userHelper.AddUserToRoleAsync(user, "Admin");
+            //}
+
+
+            // Add products
+            if (!this.context.Products.Any())
+            {
+                //this.AddProduct("iPhone X", user);
+                //this.AddProduct("Magic Mouse", user);
+                //this.AddProduct("iWatch Series 4", user);
+
+                this.AddProduct("iPhone X");
+                this.AddProduct("Magic Mouse");
+                this.AddProduct("iWatch Series 4");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        //private void AddProduct(string name, User user)
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(1000),
+                IsAvailabe = true,
+                Stock = this.random.Next(100),
+                //User = user
+            });
+        }
+    }
+}
