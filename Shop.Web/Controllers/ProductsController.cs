f76381174fdfcdaebdda1134e3a75054edbb5d3d
Marilas Software <mrilassoftware@gmail.com>
2019-03-19 20:55:50 -0500

    Se agregaron las imagenes en el editar y el crear.

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@
     using Shop.Web.Data;
     using Shop.Web.Data.Entities;
     using Shop.Web.Helpers;
+    using Shop.Web.Models;
     using System.IO;
     using System.Threading.Tasks;
 
@@ -112,25 +113,63 @@
                 return NotFound();
             }
 
-            return View(product);
+            var view = this.ToProducViewModel(product);
+
+            return View(view);
         }
+
+        private ProductViewModel ToProducViewModel(Product product)
+        {
+            return new ProductViewModel
+            {
+                Id = product.Id,
+                ImageUrl = product.ImageUrl,
+                IsAvailabe = product.IsAvailabe,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                User = product.User
+            };
+        }
+
 
         // POST: Products/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Product product)
+        public async Task<IActionResult> Edit(ProductViewModel view)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
+                    var path = view.ImageUrl;
+
+                    if (view.ImageFile != null && view.ImageFile.Length > 0)
+                    {
+                        path = Path.Combine(Directory.GetCurrentDirectory()
+                            , "wwwroot\\images\\Products"
+                            , view.ImageFile.FileName);
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await view.ImageFile.CopyToAsync(stream);
+                        }
+
+                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                    }
+
                     // TODO: Pending to change to: this.User.Identity.Name
-                    product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+                    view.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+
+                    var product = this.ToProduct(view, path);
+
                     await this.productRepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!await this.productRepository.ExistAsync(product.Id))
+                    if (!await this.productRepository.ExistAsync(view.Id))
                     {
                         return NotFound();
                     }
@@ -142,7 +181,7 @@
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(product);
+            return View(view);
         }
 
         // GET: Products/Delete/5
