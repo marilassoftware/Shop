4b0e9b22d4ab4163ee9069557962950420bb06ee
Marilas Software <mrilassoftware@gmail.com>
2019-03-19 19:54:34 -0500

    Agar images, cambios del video 15.

---
+++ Shop.Web/Data/Entities/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Shop.Web.Data.Entities
+{
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+
+}
