8b09ab289ad285c2a600e41b190aa46f67dc7dd6
Marilas Software <mrilassoftware@gmail.com>
2019-03-21 19:48:07 -0500

    Se hace la primer pantalla de Login.

---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            //this.apiService = new ApiService();
+            //this.Email = "jzuluaga55@gmail.com";
+            //this.Password = "123456";
+            //this.IsEnabled = true;
+        }
+    }
+}
